09851e2f1d0817757ddb680a1be388fb80497a28
achu2511 <achyuth2511@gmail.com>
2024-01-29 15:31:02 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import Service from './components/service'
+
+function App() {
+  // const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <Service/>
+    </>
+  )
+}
+
+export default App
